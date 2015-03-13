d4b8d877419aee6f54c29e36c064a7744e66de9e
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2015-03-13 13:56:56 +0100

    Various improvements to .tree-a component.

--- components/tree-a/js/tree-a.js
+++ components/tree-a/js/tree-a.js
@@ -14,22 +14,8 @@
 			var target = evt.data.getTarget();
 
 			// Collapse or expand item groups.
-			if ( target.is( 'h2' ) ) {
-				target[ target.hasClass( 'active' ) ? 'removeClass' : 'addClass' ]( 'active' );
-			}
-
-			// Activate atomic items.
-			if ( target.is( 'a' ) ) {
-				var parent = target.getParent(),
-					currentActive = this.findOne( 'li.active' );
-
-				if ( !parent.hasClass( 'active' ) ) {
-					parent.addClass( 'active' );
-
-					if ( currentActive ) {
-						currentActive.removeClass( 'active' );
-					}
-				}
+			if ( target.is( 'h2' ) && !target.hasClass( 'tree-a-no-sub' ) ) {
+				target[ target.hasClass( 'tree-a-active' ) ? 'removeClass' : 'addClass' ]( 'tree-a-active' );
 			}
 		} );
 	}
